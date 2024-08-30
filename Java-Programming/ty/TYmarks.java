96786c2435482e794e0c2d55f612fd85f40d12aa
Shital <telishital154@gmail.com>
2024-08-30 23:47:31 +0530

    v

---
+++ Java-Programming/ty/TYmarks.java
@@ -0,0 +1,16 @@
+package ty;
+public class TYmarks
+{
+    public int Theory;
+    public int practicals;
+    public TYmarks()
+    {
+        Theory=0;
+        practicals=0;
+    }
+    public TYmarks(int t, int p)
+    {
+        this.theory=t;
+        this.practicals=p;
+    }
+}
