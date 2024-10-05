91cb88913b4c6f6f0b93fbee2cd078708bf4e7e3
Shital <telishital154@gmail.com>
2024-10-05 18:12:42 +0530

    Logic

---
+++ Python - Programming/Activity_1.py
@@ -0,0 +1,34 @@
+# Activity 1.create empty dataframe and add record
+
+#import pandas as pd
+
+#df=pd.DataFrame(columns=['Company', 'model', 'year'])
+
+#df.loc[0]=['Tata', 'Nexon', '2017']
+#df.loc[1]=['MG', 'Astor', '2021']
+#df.loc[2]=['KIA', 'Selton', '2019']
+#df.loc[3]=['Hyundai', 'Creta', '2015']
+
+#print(df)
+
+# Activity 2.create dataframe using numpy array
+
+##import numpy as np
+##import pandas as pd
+
+##data = np.array([['Tata', 'Nexon', '2017'],
+##                 ['MG', 'Astor', '2021'],
+##                 ['KIA', 'Selton', '2019'],
+##                 ['Hyundai', 'Creta', '2015']])
+
+##df = pd.DataFrame(data, columns=['Company', 'model', 'year'])
+##print(df)
+
+
+
+
+
+
+
+
+
